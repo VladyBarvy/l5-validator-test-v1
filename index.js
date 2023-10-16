be8320ce33332c75fa7d3ee0bcbac4a7c837467d
VladyBarvy <vladbarskoff@yandex.ru>
2023-10-16 20:31:49 +0300

    step 1

--- index.js
+++ index.js
@@ -1,3 +1,30 @@
 import Validator from './src/Validator.js';
 
+
+
 export default Validator;
+
+/*
+const v = new Validator();
+const schema = v.number();
+
+console.log(schema.isValid(null)); // false
+console.log(schema.isValid('')); // false
+console.log(schema.isValid(true)); // false
+console.log(schema.isValid(123)); // true
+console.log(schema.isValid(0)); // true
+*/
+/*
+const v = new Validator();
+
+const schema1 = v.number();
+console.log(schema1.isValid(11)); // true;
+
+const schema2 = v.number().even();
+console.log(schema2.isValid(2)); // true;
+console.log(schema2.isValid(11)); // false;
+
+const schema3 = v.number().odd();
+console.log(schema3.isValid(22)); // false;
+console.log(schema3.isValid(23)); // true;
+*/
---
+++ src/Validator.js
@@ -0,0 +1,50 @@
+import NumberSchema from './NumberSchema.js';
+
+class Validator {
+    
+    number() {
+        return new NumberSchema();
+    }
+
+/*
+    number() {
+      
+        class InstanceNumberValidator {
+
+            validators = (value) => typeof value === 'number';
+          
+            isValid(value) {
+              return (this.validators(value) === true);
+            }
+
+
+            even() {
+                const validator = (value) => value % 2 === 0;
+                //this.validators.push(validator);
+                this.validators = validator;
+                return this;
+            }
+
+
+            odd() {
+                const validator = (value) => value % 2 !== 0;
+                //this.validators.push(validator);
+                this.validators = validator;
+                return this;
+            }     
+                    
+        }
+        
+        return new InstanceNumberValidator();
+    }
+*/
+
+
+
+
+
+
+    
+  }
+
+  export default Validator;
