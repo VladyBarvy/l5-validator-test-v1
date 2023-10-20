04eadae9c74964a86a6a59bb72f29874744df501
VladyBarvy <vladbarskoff@yandex.ru>
2023-10-20 19:32:11 +0300

    step 2 and 3

--- index.js
+++ index.js
@@ -12,10 +12,11 @@ console.log(schema.isValid(true)); // false
 console.log(schema.isValid(123)); // true
 console.log(schema.isValid(0)); // true
 */
-
+/*
 const v = new Validator();
 const schema = v.array();
 
 console.log(schema.isValid([]));        // true
 console.log(schema.isValid(123));       // false
 console.log(schema.isValid('Hexlet'));  // false
+*/
