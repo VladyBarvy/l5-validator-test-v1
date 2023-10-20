3f6b939ded9f42a6c82b1767fe38161b2d628132
VladyBarvy <vladbarskoff@yandex.ru>
2023-10-20 19:27:50 +0300

    step 2 and 3

--- src/ArraySchema.js
+++ src/ArraySchema.js
@@ -1,9 +1,6 @@
 export default class ArraySchema {
     validators = [(value) => Array.isArray(value) === true];
-  
     isValid(value) {
       return this.validators.every((validator) => validator(value) === true);
     }
-  
-
-  }
+}
